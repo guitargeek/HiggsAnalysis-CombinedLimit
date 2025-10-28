c74fd62bf40ebd322ad9dc7743468c0c2905760c
Massimiliano Galli <massimiliano.galli@cern.ch>
2025-10-28 14:40:47 +0100

    mimick Combine's setup to avoid std::invalid_argument

--- data/ci/exampleCreateNLL.cxx
+++ data/ci/exampleCreateNLL.cxx
@@ -1,13 +1,17 @@
+#include <cmath>
 #include <iostream>
 #include <limits>
 #include <memory>
 #include <string>
 
 #include "TFile.h"
+
 #include "RooAbsData.h"
 #include "RooAbsPdf.h"
 #include "RooAbsReal.h"
+#include "RooArgList.h"
 #include "RooArgSet.h"
+#include "RooCategory.h"
 #include "RooFitResult.h"
 #include "RooMsgService.h"
 #include "RooRealVar.h"
@@ -16,15 +20,112 @@
 
 #include "../../interface/Combine.h"
 #include "../../interface/CascadeMinimizer.h"
+#include "../../interface/RooMultiPdfCombine.h"
+#include "../../interface/utils.h"
+#include "../../interface/ProfilingTools.h"
 
 namespace {
 void printUsage(const char *argv0) {
   std::cerr << "Usage: " << argv0
             << " <workspace.root> [workspaceName=w] [modelConfig=ModelConfig] [dataName=data_obs]\n";
 }
-} // namespace
+
+bool loadSnapshotIfExists(RooWorkspace &ws, const char *name) {
+  if (!name)
+    return false;
+  if (!ws.getSnapshot(name))
+    return false;
+  return ws.loadSnapshot(name);
+}
+
+void configureCascadeMinimizerState(RooWorkspace &ws, RooStats::ModelConfig &mc) {
+  auto &cfg = CascadeMinimizerGlobalConfigs::O();
+
+  cfg.parametersOfInterest = RooArgList();
+  if (const RooArgSet *poi = mc.GetParametersOfInterest()) {
+    for (RooAbsArg *arg : *poi) {
+      auto *var = dynamic_cast<RooRealVar *>(arg);
+      if (!var)
+        continue;
+      cfg.parametersOfInterest.add(*var);
+    }
+  }
+
+  cfg.nuisanceParameters = RooArgList();
+  if (const RooArgSet *nuis = mc.GetNuisanceParameters()) {
+    for (RooAbsArg *arg : *nuis) {
+      auto *var = dynamic_cast<RooRealVar *>(arg);
+      if (!var)
+        continue;
+      if (!var->isConstant())
+        cfg.nuisanceParameters.add(*var);
+    }
+  }
+
+  cfg.allFloatingParameters = RooArgList();
+  RooArgSet allVars(ws.allVars());
+  for (RooAbsArg *arg : allVars) {
+    auto *var = dynamic_cast<RooRealVar *>(arg);
+    if (!var)
+      continue;
+    if (!var->isConstant())
+      cfg.allFloatingParameters.add(*var);
+  }
+
+  cfg.pdfCategories = RooArgList();
+  cfg.allRooMultiPdfParams = RooArgList();
+  cfg.allRooMultiPdfs = RooArgList();
+
+  std::unique_ptr<RooArgSet> discreteParameters{
+      dynamic_cast<RooArgSet *>(ws.genobj("discreteParams"))};
+
+  if (discreteParameters) {
+    for (RooAbsArg *arg : *discreteParameters) {
+      auto *cat = dynamic_cast<RooCategory *>(arg);
+      if (!cat)
+        continue;
+      if (!cfg.pdfCategories.containsInstance(*cat))
+        cfg.pdfCategories.add(*cat);
+    }
+  } else if (runtimedef::get("ADD_DISCRETE_FALLBACK")) {
+    RooArgSet categories(ws.allCats());
+    for (RooAbsArg *arg : categories) {
+      auto *cat = dynamic_cast<RooCategory *>(arg);
+      if (!cat)
+        continue;
+      if (std::string(cat->GetName()).find("pdfindex") == std::string::npos)
+        continue;
+      if (!cfg.pdfCategories.containsInstance(*cat))
+        cfg.pdfCategories.add(*cat);
+    }
+  }
+
+  if (cfg.pdfCategories.getSize() > 0) {
+    RooArgSet clients;
+    utils::getClients(cfg.pdfCategories, ws.allPdfs(), clients);
+    for (RooAbsArg *arg : clients) {
+      auto *multi = dynamic_cast<RooMultiPdf *>(arg);
+      if (!multi)
+        continue;
+      if (!cfg.allRooMultiPdfs.containsInstance(*multi))
+        cfg.allRooMultiPdfs.add(*multi);
+
+      std::unique_ptr<RooArgSet> pdfParams{multi->getParameters((const RooArgSet *)nullptr)};
+      for (RooAbsArg *a : *pdfParams) {
+        auto *var = dynamic_cast<RooRealVar *>(a);
+        if (!var || var->isConstant())
+          continue;
+        if (!cfg.allRooMultiPdfParams.containsInstance(*var))
+          cfg.allRooMultiPdfParams.add(*var);
+      }
+    }
+  }
+}
+}  // namespace
 
 int main(int argc, char **argv) {
+  CascadeMinimizer::initOptions();
+
   if (argc < 2) {
     printUsage(argv[0]);
     return 1;
@@ -47,6 +148,9 @@ int main(int argc, char **argv) {
     file->ls();
     return 2;
   }
+
+  utils::check_inf_parameters(workspace->allVars(), /*verbosity=*/0);
+  loadSnapshotIfExists(*workspace, "clean");
 
   auto *modelConfig =
       dynamic_cast<RooStats::ModelConfig *>(workspace->genobj(modelConfigName.c_str()));
@@ -72,6 +176,19 @@ int main(int argc, char **argv) {
   RooMsgService::instance().setGlobalKillBelow(RooFit::ERROR);
   RooAbsReal::setEvalErrorLoggingMode(RooAbsReal::CountErrors);
 
+  const RooArgSet *poi = modelConfig->GetParametersOfInterest();
+  if (!poi || poi->getSize() == 0) {
+    std::cerr << "ERROR: ModelConfig '" << modelConfigName << "' has no parameters of interest.\n";
+    return 2;
+  }
+  for (RooAbsArg *arg : *poi) {
+    auto *var = dynamic_cast<RooRealVar *>(arg);
+    if (var)
+      var->setConstant(false);
+  }
+
+  configureCascadeMinimizerState(*workspace, *modelConfig);
+
   const RooArgSet *nuisances = modelConfig->GetNuisanceParameters();
   RooArgSet constraintSet;
   const RooArgSet *constraintPtr = nullptr;
@@ -86,17 +203,7 @@ int main(int argc, char **argv) {
     return 3;
   }
 
-  const RooArgSet *poi = modelConfig->GetParametersOfInterest();
-  RooRealVar *primaryPoi = nullptr;
-  if (poi) {
-    for (RooAbsArg *arg : *poi) {
-      if (auto *var = dynamic_cast<RooRealVar *>(arg)) {
-        var->setConstant(false);
-        if (primaryPoi == nullptr)
-          primaryPoi = var;
-      }
-    }
-  }
+  RooRealVar *primaryPoi = dynamic_cast<RooRealVar *>(poi->first());
 
   std::cout << "Initial NLL value: " << nll->getVal() << '\n';
 
@@ -114,36 +221,36 @@ int main(int argc, char **argv) {
   if (fitResult) {
     std::cout << "Minimizer status: " << fitResult->status()
               << ", edm=" << fitResult->edm() << '\n';
-  }
-
-  if (poi && poi->getSize() > 0) {
-    std::cout << "Best-fit POI values:\n";
-    for (RooAbsArg *arg : *poi) {
-      auto *poiVar = dynamic_cast<RooRealVar *>(arg);
-      if (!poiVar)
-        continue;
-
-      const double val = poiVar->getVal();
-      double errHi = std::numeric_limits<double>::quiet_NaN();
-      double errLo = std::numeric_limits<double>::quiet_NaN();
-      if (fitResult) {
-        if (auto *fitVar = dynamic_cast<RooRealVar *>(fitResult->floatParsFinal().find(poiVar->GetName()))) {
-          errHi = fitVar->getErrorHi();
-          errLo = fitVar->getErrorLo();
-        }
-      }
-
-      std::cout << "  " << poiVar->GetName() << " = " << val;
-      if (fitResult) {
-        std::cout << " +" << errHi << " / " << errLo;
-      }
-      std::cout << '\n';
-    }
   } else {
-    std::cout << "ModelConfig has no parameters of interest.\n";
+    std::cout << "WARNING: RooFitResult unavailable (minimizer did not provide one).\n";
   }
 
-  std::cout << "Finished building and evaluating the NLL from workspace '" << workspaceName
+  std::cout << "Best-fit POI values:\n";
+  for (RooAbsArg *arg : *poi) {
+    auto *poiVar = dynamic_cast<RooRealVar *>(arg);
+    if (!poiVar)
+      continue;
+
+    const double val = poiVar->getVal();
+    double errHi = std::numeric_limits<double>::quiet_NaN();
+    double errLo = std::numeric_limits<double>::quiet_NaN();
+
+    if (fitResult) {
+      if (auto *fitVar =
+              dynamic_cast<RooRealVar *>(fitResult->floatParsFinal().find(poiVar->GetName()))) {
+        errHi = fitVar->getAsymErrorHi();
+        errLo = fitVar->getAsymErrorLo();
+      }
+    }
+
+    std::cout << "  " << poiVar->GetName() << " = " << val;
+    if (!std::isnan(errHi) && !std::isnan(errLo)) {
+      std::cout << " +" << errHi << " / " << errLo;
+    }
+    std::cout << '\n';
+  }
+
+  std::cout << "Finished building and minimising the NLL from workspace '" << workspaceName
             << "'.\n";
   return 0;
 }
